83fa6fbc5f622d81d930d44cb5f3b402de392dd0
Adonys Santos <adonysmundotech@gmail.com>
2021-07-02 06:00:11 -0400

    Add player

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Hello!");
+const videoPlayer = document.querySelector("#player");
+const playButton = document.querySelector("#playButton");
+const pauseButton = document.querySelector("#pauseButton");
+
+function MediaPlayer(config) {
+  this.media = config.element;
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+const player = new MediaPlayer({ element: videoPlayer });
+playButton.onclick = () => player.play();
+pauseButton.onclick = () => player.pause();
