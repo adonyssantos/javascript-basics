fca4f7e75b19da32e48d97ad52533c36f589d9c2
Adonys Santos <adonysmundotech@gmail.com>
2021-07-02 07:00:50 -0400

    Add MediaPlayer module

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
+import MediaPlayer from "./lib/MediaPlayer";
+
 const videoPlayer = document.querySelector("#player");
 const playButton = document.querySelector("#playButton");
 const pauseButton = document.querySelector("#pauseButton");
-
-function MediaPlayer(config) {
-  this.media = config.element;
-}
-
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
 
 const player = new MediaPlayer({ element: videoPlayer });
 playButton.onclick = () => player.play();
