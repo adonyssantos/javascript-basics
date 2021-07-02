73bc2c8c6face0f0d964fdb60c19babace8ffa10
Adonys Santos <adonysmundotech@gmail.com>
2021-07-02 08:00:46 -0400

    Add toggleMute

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import AutoPlay from "./lib/plugins/AutoPlay.js";
 
 const videoPlayer = document.querySelector("#player");
 const playButton = document.querySelector("#playButton");
+const muteButton = document.querySelector("#muteButton");
 
 const player = new MediaPlayer({
   element: videoPlayer,
   plugins: [new AutoPlay()],
 });
 playButton.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
