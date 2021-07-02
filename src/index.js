8e0e8862a655796185094e088cdd399b54957f02
Adonys Santos <adonysmundotech@gmail.com>
2021-07-02 07:42:01 -0400

    Add AutoPlay plugin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import MediaPlayer from "./lib/MediaPlayer";
+import MediaPlayer from "./lib/MediaPlayer.js";
+import AutoPlay from "./lib/plugins/AutoPlay.js";
 
 const videoPlayer = document.querySelector("#player");
 const playButton = document.querySelector("#playButton");
 const pauseButton = document.querySelector("#pauseButton");
 
-const player = new MediaPlayer({ element: videoPlayer });
+const player = new MediaPlayer({
+  element: videoPlayer,
+  plugins: [new AutoPlay()],
+});
 playButton.onclick = () => player.play();
 pauseButton.onclick = () => player.pause();
